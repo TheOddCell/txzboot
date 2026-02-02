cfc751ac19ae53fafef77b95d98e13afe95716d5
TheOddCell <me@oddcell.ca>
2026-02-02 17:00:19 -0500

    debug shells

--- txzboot.sh
+++ txzboot.sh
@@ -19,6 +19,7 @@ cd ../..
 cat > rootfs/init << 'EOF'
 #!/bin/sh
 export PATH="/bin"
+exec </dev/console >/dev/console 2>&1
 echo "txzboot.loader"
 
 mkdir -p /proc /sys /dev /mnt /mnt/proc /mnt/sys /mnt/dev
@@ -27,6 +28,15 @@ mount -t proc proc /proc
 mount -t sysfs sys /sys
 mount -t devtmpfs dev /dev
 
+if ! [ -f /boot.txz ]; then exec sh; fi
+#trap 'exec sh' TSTP
+i=5
+while [ $i -gt 0 ]; do
+  printf '\rPress any key within %d second(s) to get a shell... ' "$i"
+  read -r -n 1 -t 1 _ && { echo; exec sh; break; }
+  i=$((i-1))
+done
+echo "                                                     "
 tar -vxJf /boot.txz -C /mnt
 
 mount -t proc proc /mnt/proc
@@ -34,14 +44,20 @@ mount -t sysfs sys /mnt/sys
 mount -t devtmpfs dev /mnt/dev
 
 echo "txzboot.loader ready"
+i=5
+while [ $i -gt 0 ]; do
+  printf '\rPress any key within %d second(s) to get a shell... ' "$i"
+  read -r -n 1 -t 1 _ && { echo; exec sh; break; }
+  i=$((i-1))
+done
+echo "                                                     "
 
 [ -f /mnt/init ] && exec chroot /mnt /init
 [ -f /mnt/linuxrc ] && exec chroot /mnt /linuxrc
 [ -f /mnt/sbin/init ] && exec chroot /mnt /sbin/init
 [ -f /mnt/bin/init ] && exec chroot /mnt /bin/init
 
-echo "ERROR: No valid init found. Type path or Ctrl+D to drop to shell."
-echo "       Use the command 'proper' to get an init which miiiiiiiight work in a pinch."
+echo "ERROR: No valid init found. Type path or Ctrl+D to drop to a shell."
 
 if ! read nextsteps; then echo "Good luck"; exec sh; fi
 [ "${nextsteps#/}" = "$nextsteps" ] && nextsteps="/$nextsteps"
@@ -53,10 +69,11 @@ exec sh
 EOF
 chmod a+x rootfs/init
 echo "txzboot.loader installed"
-cp "$1" rootfs/boot.txz
-echo "boot.txz added"
+if [ "$1" != '--shellonly' ]; then
+  cp "$1" rootfs/boot.txz
+  echo "boot.txz added"
+fi
 cd rootfs
-#bash
 find . -print0 \
  | cpio --null -ov --format=newc \
  | zstd -19 -T0 > ../initramfs-full.cpio.zst
