befcc3e5801203f7ef433d835aac6f5d6a84a60c
TheOddCell <me@oddcell.ca>
2026-02-10 19:18:49 -0500

    customcpio

--- txzboot.maker.sh
+++ txzboot.maker.sh
@@ -5,44 +5,50 @@ if [ -z "$1" ]; then
   exit 1
 fi
 echo "txzboot.maker"
-mkdir rootfs
-mkdir rootfs/bin
-echo "<[busybox]>" | base64 -d > rootfs/bin/busybox
-chmod a+x rootfs/bin/busybox
-mkdir rootfs/etc
-ln -s ../proc/self/mounts rootfs/etc/mtab
-echo "txzboot.loader dependancies installed"
-cd rootfs/bin
-./busybox --install .
-cd ../..
-echo "<[txzboot.loader]>"    | base64 -d > rootfs/bin/txzboot.loader
-ln rootfs/bin/txzboot.loader rootfs/init
-chmod a+x rootfs/init
-echo "txzboot.loader installed"
-if [ "$1" != 'shell' ]; then
-  if [ "${1##*.}" = "xz" ]; then
-    cp "$1" rootfs/boot.txz
-    echo "boot.txz added"
-  elif [ "${1##*.}" = "gz" ]; then
-    cp "$1" rootfs/boot.tgz
-    echo "boot.tgz added"
-  else
-    cp "$1" rootfs/boot.tar
-    echo "boot.tar added"
+CPIOPATH="initramfs-full.cpio.zst"
+if [[ "$1" != *"cpio"* ]]; then
+  mkdir rootfs
+  mkdir rootfs/bin
+  echo "<[busybox]>" | base64 -d > rootfs/bin/busybox
+  chmod a+x rootfs/bin/busybox
+  mkdir rootfs/etc
+  ln -s ../proc/self/mounts rootfs/etc/mtab
+  echo "txzboot.loader dependancies installed"
+  cd rootfs/bin
+  ./busybox --install .
+  cd ../..
+  echo "<[txzboot.loader]>"    | base64 -d > rootfs/bin/txzboot.loader
+  ln rootfs/bin/txzboot.loader rootfs/init
+  chmod a+x rootfs/init
+  echo "txzboot.loader installed"
+  if [ "$1" != 'shell' ]; then
+    if [ "${1##*.}" = "xz" ]; then
+      cp "$1" rootfs/boot.txz
+        echo "boot.txz added"
+    elif [ "${1##*.}" = "gz" ]; then
+      cp "$1" rootfs/boot.tgz
+      echo "boot.tgz added"
+    else
+      cp "$1" rootfs/boot.tar
+      echo "boot.tar added"
+    fi
   fi
+  cd rootfs
+  find . -print0 \
+   | cpio --null -o --format=newc \
+   | zstd -19 -T0 > ../initramfs-full.cpio.zst
+  cd ..
+else
+  CPIOPATH="$1"
 fi
-cd rootfs
-find . -print0 \
- | cpio --null -o --format=newc \
- | zstd -19 -T0 > ../initramfs-full.cpio.zst
-cd ..
 echo "<[vmlinuz]>" | base64 -d > vmlinuz.tmp
 ukify build \
   --linux vmlinuz.tmp \
-  --initrd initramfs-full.cpio.zst \
+  --initrd "$CPIOPATH" \
   --cmdline "rw" --output "txzboot.uki.efi"
+rm vmlinuz.tmp
 echo "txzboot.loader created"
-echo "Cleaning up..."
 if [ "$2" != "--nocleanup" ]
-  rm -rf rootfs initramfs-full.cpio.zst vmlinuz.tmp
+  echo "Cleaning up..."
+  rm -rf rootfs initramfs-full.cpio.zst
 fi
